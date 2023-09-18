6ad008aed1a8b16f5d40a2fa458cf23d0a60a63d
userlxw666 <lxw1378045191@163.com>
2023-09-18 13:18:05 +0800

    category server

--- app/category/server/server.go
+++ app/category/server/server.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 	"errors"
-	"fmt"
 	"micro_shopping/app/category/dao"
 	"micro_shopping/app/category/dao/model"
 	"micro_shopping/idl/pb"
@@ -23,34 +22,6 @@ func (cgsv *CategoryService) CreateCategory(ctx context.Context, req *pb.Categor
 	categoryDao.CreateCategory(category)
 	resp.Name = req.Name
 	resp.Desc = req.Desc
-	return resp, nil
-}
-
-func (cgsv *CategoryService) BulkCreateCategory(ctx context.Context, req *pb.BulkRequest) (resp *pb.BulkResponse, err error) {
-	resp = new(pb.BulkResponse)
-	result := req.GetBulkRequest()
-	categoryDao := CategoryDao.NewCategoryDao(ctx)
-	var categories = make([]model.Category, 0)
-	rp := resp.GetBulkResponse()
-	// 判断该种类是否已经存在
-	for _, request := range result {
-		cat := categoryDao.FindByName(request.Name)
-		if cat != nil {
-			fmt.Println("categories exist", err)
-			return resp, err
-		}
-		categories = append(categories, *model.NewCategory(request.Name, request.Desc))
-		rp = append(rp, &pb.CategoryResponse{
-			Name: request.Name,
-			Desc: request.Desc,
-		})
-	}
-	// 存入数据库
-	_, err = categoryDao.CreateAllCategory(categories)
-	if err != nil {
-		fmt.Println("批量创建失败", err)
-		return nil, err
-	}
 	return resp, nil
 }
 
